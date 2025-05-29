cac45058e947dbb76b63bd79fa151e4fe31f2519
Subham Lamichhane <lamichhanesubham4@gmail.com>
2025-05-29 04:49:46 -0400

    Add files via upload

---
+++ while True condition.py
@@ -0,0 +1,7 @@
+while True :
+    userinput=input("Enter your city name :")
+    if userinput == "quit" :
+      break 
+    else :
+       print(f"I would love to go to {userinput.title()}")
+      
